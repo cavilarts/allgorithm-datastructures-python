12e827cb689bd047acd8d1a23870153e547922ce
Carlos Avila <cavilart21@gmail.com>
2023-12-12 14:29:44 +0100

    starting with basic implementation of factorials in its iterative and recursive form

---
+++ algorithms/easy/factorials.py
@@ -0,0 +1,29 @@
+
+""" 
+  Iterative approach
+  calculate_factorial
+  @params n = int
+  @return int
+"""
+def calculate_factorial(n: int) -> int:
+  total = 1
+
+  for i in range(2, n + 1):
+    total *= i
+
+  return total
+""" 
+  Recursive approach
+  calc_factorial
+  @params n = int
+  @return int
+"""
+def calc_factorial(n: int) -> int:
+  if n == 1:
+    return n
+  
+  return n * calc_factorial(n - 1)
+
+
+print(calc_factorial(6))
+print(calculate_factorial(6))
