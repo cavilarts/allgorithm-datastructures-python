fb8bfe77f3f03f06de164afc8c868230cd98b10f
Carlos Avila <cavilart21@gmail.com>
2023-12-14 15:00:51 +0100

    using a naive implementation of matrix multiplication

---
+++ algorithms/medium/matrix_multiplication.py
@@ -0,0 +1,15 @@
+X = [[1, 2],
+     [2, 3]]
+
+Y = [[2, 3],
+     [3, 4]]
+
+result = [[0, 0],
+          [0, 0]]
+
+for i in range(len(X)):
+  for j in range(len(Y[0])):
+    for k in range(len(Y)):
+      result[i][j] += X[i][k] * Y[k][j]
+      
+print(result)
